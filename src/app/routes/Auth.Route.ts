cf0cfb480bbf36653fc897081ce054c98048892e
Kocoto <duoc6694@gmail.com>
2025-03-28 16:25:33 +0700

    refactor: separate build and start scripts, improve auth flow
    - Split `start` script into `build` and `start` for better separation of concerns
    - Add response for successful password change in AuthController
    - Reorder routes for better organization and fix method for reset-password
    - Temporarily disable OTP verification logic in AuthService for testing

--- src/app/routes/Auth.Route.ts
+++ src/app/routes/Auth.Route.ts
@@ -4,13 +4,14 @@ import { checkLogin } from "../middlewares/Auth.Middleware";
 
 const router = Router();
 
+router.post("/change-password", checkLogin, AuthControllers.changePassword);
 router.post("/verify-otp-to-reset-password", AuthControllers.verifyOTP);
 router.post("/verify-otp-to-login", AuthControllers.verifyOTPtoLogin);
-router.put("/reset-password", AuthControllers.resetPassword);
+router.post("/reset-password", AuthControllers.resetPassword);
 router.post("/refresh-token", AuthControllers.refreshToken);
-router.post("/logout", AuthControllers.logout);
-router.post("/resend-otp", AuthControllers.getOTP);
 router.post("/register", AuthControllers.register);
+router.post("/resend-otp", AuthControllers.getOTP);
+router.post("/logout", AuthControllers.logout);
 router.post("/login", AuthControllers.login);
 
 export default router;
