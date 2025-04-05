d1283d16081e00be364784af877a5ebb89b5563d
Kocoto <duoc6694@gmail.com>
2025-04-05 14:24:46 +0700

    feat(ProfileService): return user data with profile to enhance profile retrieval
    This change modifies the `getProfile` method in `ProfileService` to include user data in the response. It also ensures that the user exists before fetching the profile, improving error handling and data consistency.
    
    feat(AuthService): add phone number validation during user registration
    
    The `register` method in `AuthService` now checks for existing phone numbers during user registration to prevent duplicate entries, enhancing data integrity and user experience.

--- src/app/services/Profile.Service.ts
+++ src/app/services/Profile.Service.ts
@@ -1,11 +1,19 @@
 import profileModel from "../models/Profile.Model";
+import UserModel from "../models/User.Model";
 import CustomError from "../utils/Error.Util";
 class ProfileService {
   async getProfile(userId: string) {
     try {
+      const user = await UserModel.findById(userId);
+      if (!user) {
+        throw new CustomError(404, "Không tìm thấy người dùng");
+      }
       const profile = await profileModel.findOne({ userId: userId });
+      if (!profile) {
+        return null;
+      }
 
-      return profile;
+      return { profile, user };
     } catch (error) {
       if (error instanceof CustomError) {
         throw error;
