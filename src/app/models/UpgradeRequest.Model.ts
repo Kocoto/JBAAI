7330c0b0d9a17c3d32f4a3b779cc1d753bbfbf3d
Kocoto <duoc6694@gmail.com>
2025-04-10 15:00:43 +0700

    feat(upgrade-request): add service and update model for upgrade requests
    Introduce a new service to handle creation and retrieval of upgrade requests. Additionally, update the UpgradeRequest model to include a new status "reviewing" and add timestamps for better tracking. These changes enhance the functionality and maintainability of the upgrade request feature.

--- src/app/models/UpgradeRequest.Model.ts
+++ src/app/models/UpgradeRequest.Model.ts
@@ -8,16 +8,32 @@ interface IUpgradeRequest {
   fullname: string;
   address: string;
   role: "spa" | "doctor" | "pharmacy" | "school" | "hospital";
-  status: "pending" | "approved" | "rejected";
+  status: "pending" | "approved" | "rejected" | "reviewing";
 }
 
-const UpgradeRequestSchema = new Schema<IUpgradeRequest>({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  salerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  phone: { type: String, required: true },
-  email: { type: String, required: true },
-  fullname: { type: String, required: true },
-  role: { type: String, required: true },
-  address: { type: String, required: true },
-  status: { type: String, default: "pending" },
-});
+const UpgradeRequestSchema = new Schema<IUpgradeRequest>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    salerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+    fullname: { type: String, required: true },
+    role: { type: String, required: true },
+    address: { type: String, required: true },
+    status: { type: String, default: "pending" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UpgradeRequestModel = mongoose.model<IUpgradeRequest>(
+  "UpgradeRequest",
+  UpgradeRequestSchema
+);
+
+export default UpgradeRequestModel;
