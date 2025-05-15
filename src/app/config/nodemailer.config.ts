5ebc5c06ed3f62db95372ee391d6a54999b6875c
Kocoto <duoc6694@gmail.com>
2025-05-15 09:37:22 +0700

    chore: update email credentials and nodemailer configuration
    Update the email user and password in the .env file and modify the nodemailer configuration to use Outlook's SMTP server instead of Gmail. These changes ensure the email service is properly configured for the new environment.

--- src/app/config/nodemailer.config.ts
+++ src/app/config/nodemailer.config.ts
@@ -1,7 +1,9 @@
 import * as nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
