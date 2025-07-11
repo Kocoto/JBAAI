35592df42a49ab1394cab0b2f62b4fba37d9b75a
Kocoto <duoc6694@gmail.com>
2025-07-11 16:56:59 +0700

    refactor(routes): remove unused test route from report routes

--- src/app/routes/Report.Route.ts
+++ src/app/routes/Report.Route.ts
@@ -5,6 +5,5 @@ import { checkLogin } from "../middlewares/Auth.Middleware";
 const router = Router();
 
 router.get("/revenue", ReportController.exportRevenueReport);
-router.get("/test", checkLogin, ReportController.test);
 
 export default router;
